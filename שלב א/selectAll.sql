fe69ace7265478ad76896ee132ab7f96a3a4a085
shiri shahor <shirishahor@gmail.com>
2026-03-21 22:10:40 +0200

    neat

---
+++ שלב א/selectAll.sql
@@ -0,0 +1,29 @@
+-- =========================
+-- LAB_ORDER
+-- =========================
+SELECT * FROM LAB_ORDER;
+
+-- =========================
+-- LAB_TEST
+-- =========================
+SELECT * FROM LAB_TEST;
+
+-- =========================
+-- DIAGNOSTIC_EQUIPMENT
+-- =========================
+SELECT * FROM DIAGNOSTIC_EQUIPMENT;
+
+-- =========================
+-- LAB_TECHNICIAN
+-- =========================
+SELECT * FROM LAB_TECHNICIAN;
+
+-- =========================
+-- LAB_ORDER_TEST
+-- =========================
+SELECT * FROM LAB_ORDER_TEST;
+
+-- =========================
+-- LAB_RESULT
+-- =========================
+SELECT * FROM LAB_RESULT;
