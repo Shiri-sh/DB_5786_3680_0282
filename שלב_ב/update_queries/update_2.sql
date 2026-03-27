73c92ff34ba380b5d39996ffa9a72821423b9b6e
shiri shahor <shirishahor@gmail.com>
2026-03-27 14:53:36 +0300

    update_2

--- שלב_ב/update_queries/update_2.sql
+++ שלב_ב/update_queries/update_2.sql
@@ -1,4 +1,16 @@
 -- Update the description of lab tests that cost more than $1000 to indicate that they are expensive.
+
+-- before update
+SELECT test_id, description, cost
+FROM LAB_TEST
+WHERE cost > 400;
+
+-- update
 UPDATE LAB_TEST
 SET description = description || ' (Expensive Test)'
-WHERE cost > 1000;
+WHERE cost > 400;
+
+-- after update
+SELECT test_id, description, cost
+FROM LAB_TEST
+WHERE cost > 400;
