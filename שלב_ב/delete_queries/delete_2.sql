7dcf9d22afafa00e3b622815317e1ceb72bc8b30
shiri shahor <shirishahor@gmail.com>
2026-03-27 14:52:33 +0300

    delete_2

--- שלב_ב/delete_queries/delete_2.sql
+++ שלב_ב/delete_queries/delete_2.sql
@@ -2,5 +2,14 @@
 -- Delete diagnostic equipment that has not been maintained in the last 5 years.
 -- There are too old and may not be safe to use, and removing them can help to free up storage space and reduce clutter in the database.
 
+-- before deletion
+SELECT * FROM DIAGNOSTIC_EQUIPMENT
+WHERE maintenance_date < CURRENT_DATE - INTERVAL '5 years';
+
+-- deletion
 DELETE FROM DIAGNOSTIC_EQUIPMENT
+WHERE maintenance_date < CURRENT_DATE - INTERVAL '5 years';
+
+-- after deletion
+SELECT * FROM DIAGNOSTIC_EQUIPMENT
 WHERE maintenance_date < CURRENT_DATE - INTERVAL '5 years';
