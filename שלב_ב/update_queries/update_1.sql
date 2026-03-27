2fa5498cafca38c0bc5cc54e4a51fe58edda4974
shiri shahor <shirishahor@gmail.com>
2026-03-27 14:52:50 +0300

    update_1

--- שלב_ב/update_queries/update_1.sql
+++ שלב_ב/update_queries/update_1.sql
@@ -1,5 +1,15 @@
 
 -- Update the status of lab orders to 'COMPLETED' if all associated tests have results.
+
+--before update
+SELECT o.lab_order_id, o.status
+FROM LAB_ORDER o
+JOIN LAB_ORDER_TEST ot ON o.lab_order_id = ot.lab_order_id
+LEFT JOIN LAB_RESULT r ON ot.lab_order_test_id = r.lab_order_test_id
+GROUP BY o.lab_order_id, o.status
+HAVING COUNT(ot.test_id) = COUNT(r.result_id);
+
+-- update
 UPDATE LAB_ORDER
 SET status = 'COMPLETED'
 WHERE lab_order_id IN (
@@ -10,3 +20,11 @@ WHERE lab_order_id IN (
     GROUP BY o.lab_order_id
     HAVING COUNT(ot.test_id) = COUNT(r.result_id)
 );
+
+-- after update
+SELECT o.lab_order_id, o.status
+FROM LAB_ORDER o
+JOIN LAB_ORDER_TEST ot ON o.lab_order_id = ot.lab_order_id
+LEFT JOIN LAB_RESULT r ON ot.lab_order_test_id = r.lab_order_test_id
+GROUP BY o.lab_order_id, o.status
+HAVING COUNT(ot.test_id) = COUNT(r.result_id);
