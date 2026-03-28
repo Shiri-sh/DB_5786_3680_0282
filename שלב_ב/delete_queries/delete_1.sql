9dae100927ab01fb6aba607c8fe8b5bf0e93eb9b
shiri shahor <shirishahor@gmail.com>
2026-03-29 02:13:12 +0300

    delete_1

--- שלב_ב/delete_queries/delete_1.sql
+++ שלב_ב/delete_queries/delete_1.sql
@@ -1,5 +1,39 @@
--- Delete all lab orders that were placed more than a year ago and are still in 'ORDERED' status.
+-- Delete all lab orders that were placed more than two years ago and are still in 'ORDERED' status.
 --  This helps to clean up old orders that were never completed or canceled, and free up storage space in
+
+-- BEFORE
+SELECT * FROM LAB_ORDER
+WHERE status = 'ORDERED'
+AND order_date < CURRENT_DATE - INTERVAL '2 year';
+
+-- delete from deepest table first
+DELETE FROM LAB_RESULT
+WHERE lab_order_test_id IN (
+    SELECT lab_order_test_id
+    FROM LAB_ORDER_TEST
+    WHERE lab_order_id IN (
+        SELECT lab_order_id
+        FROM LAB_ORDER
+        WHERE status = 'ORDERED'
+        AND order_date < CURRENT_DATE - INTERVAL '2 year'
+    )
+);
+
+-- delete from middle table
+DELETE FROM LAB_ORDER_TEST
+WHERE lab_order_id IN (
+    SELECT lab_order_id
+    FROM LAB_ORDER
+    WHERE status = 'ORDERED'
+    AND order_date < CURRENT_DATE - INTERVAL '2 year'
+);
+
+-- delete from main table
 DELETE FROM LAB_ORDER
 WHERE status = 'ORDERED'
-AND order_date < CURRENT_DATE - INTERVAL '1 year';
+AND order_date < CURRENT_DATE - INTERVAL '2 year';
+
+-- AFTER
+SELECT * FROM LAB_ORDER
+WHERE status = 'ORDERED'
+AND order_date < CURRENT_DATE - INTERVAL '2 year';
