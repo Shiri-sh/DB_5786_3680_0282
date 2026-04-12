d02144d708b85d7effa2a3674034ffa95a51422e
yaeliShushan <ys0583299134@gmail.com>
2026-04-12 12:48:15 +0300

    Simplify query by removing BETWEEN approach
    Removed the second approach for filtering order date using BETWEEN.

--- שלב_ב/select_queries/select_6.sql
+++ שלב_ב/select_queries/select_6.sql
@@ -6,10 +6,3 @@ FROM LAB_ORDER
 WHERE priority = 'URGENT'
 AND status != 'COMPLETED'
 AND order_date < CURRENT_DATE - INTERVAL '2 days';
-
--- second approach is to use BETWEEN to filter the order date
-SELECT *
-FROM LAB_ORDER
-WHERE priority = 'URGENT'
-AND status != 'COMPLETED'
-AND order_date BETWEEN DATE '1900-01-01' AND CURRENT_DATE - INTERVAL '2 days';
