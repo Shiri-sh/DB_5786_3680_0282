2b40488581ab2d93b3fe4c9fbf83843b7a12d0f9
shiri shahor <shirishahor@gmail.com>
2026-04-15 15:30:51 +0300

    postgres optimize

--- שלב_א/init-db/02_load_all.sql
+++ שלב_א/init-db/02_load_all.sql
@@ -1,12 +1,10 @@
 
 
--- insert base
+\i '/docker-entrypoint-initdb.d/03_DataImportFiles/load_equipment.sql'
+
 \i '/docker-entrypoint-initdb.d/02_insert_base/insert_lab_order.sql'
 \i '/docker-entrypoint-initdb.d/02_insert_base/insert_lab_technician.sql'
 \i '/docker-entrypoint-initdb.d/02_insert_base/insert_lab_test.sql'
-
--- load equipment
-\i '/docker-entrypoint-initdb.d/03_DataImportFiles/load_equipment.sql'
 
 -- relations
 \i '/docker-entrypoint-initdb.d/04_insert_relations/insert_order_tests.sql'
