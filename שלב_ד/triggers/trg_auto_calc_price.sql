4e395b2235127ea5fedcaaa0eadea4e307faba7d
Your Name <ys0583299134@gmail.com>
2026-05-20 14:10:13 +0300

    pics

--- שלב_ד/triggers/trg_auto_calc_price.sql
+++ שלב_ד/triggers/trg_auto_calc_price.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION fn_trg_update_price() 
 RETURNS TRIGGER AS $$
 BEGIN
-    UPDATE LAB_ORDER 
+    UPDATE labs.LAB_ORDER 
     SET total_price = fn_calculate_order_total(NEW.lab_order_id)
     WHERE lab_order_id = NEW.lab_order_id;
     RETURN NEW;
@@ -14,14 +14,14 @@ FOR EACH ROW EXECUTE FUNCTION fn_trg_update_price();
 
 -- before insert
 SELECT lab_order_id, total_price 
-FROM LAB_ORDER 
+FROM labs.LAB_ORDER 
 WHERE lab_order_id = 1;
 
 -- insert
-INSERT INTO LAB_ORDER_TEST (lab_order_id, test_id) 
+INSERT INTO labs.LAB_ORDER_TEST (lab_order_id, test_id) 
 VALUES (1, 2);
 
 -- after insert
 SELECT lab_order_id, total_price 
-FROM LAB_ORDER 
+FROM labs.LAB_ORDER 
 WHERE lab_order_id = 1;
